2daf7b52056bbfd9ca6071c63e87a770a1e5cb12
lch <lvcunhao@hxdi.com>
2026-08-30 16:47:34 +0800

    修复bug：写错服务名称

--- hm-api/src/main/java/com/hmall/api/feignclient/OrderClient.java
+++ hm-api/src/main/java/com/hmall/api/feignclient/OrderClient.java
@@ -3,9 +3,8 @@ package com.hmall.api.feignclient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "order-service")
+@FeignClient(name = "trade-service")
 public interface OrderClient {
 
     @PutMapping("/orders/{orderId}")
