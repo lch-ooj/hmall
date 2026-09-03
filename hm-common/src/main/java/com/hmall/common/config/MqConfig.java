081fb1003933eaccda5e5ad5ea2220e2898aa4f1
lch <lvcunhao@hxdi.com>
2026-09-03 10:50:07 +0800

    RabbitMQ消息发送工具类。

--- hm-common/src/main/java/com/hmall/common/config/MqConfig.java
+++ hm-common/src/main/java/com/hmall/common/config/MqConfig.java
@@ -1,19 +1,34 @@
 package com.hmall.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hmall.common.utils.RabbitMqHelper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass(value = {MessageConverter.class, RabbitTemplate.class})
 public class MqConfig {
 
     /**
-     * 配置消息转换器
-     * @return
+     * 消息转换器
      */
     @Bean
-    public MessageConverter messageConverter(){
-        return new Jackson2JsonMessageConverter();
+    @ConditionalOnBean(ObjectMapper.class) //条件装配：只有当容器中存在 ObjectMapper Bean 时才生效。 Jackson 转换器只在 Jackson 依赖存在时才创建，避免项目没有 Jackson 时报错。
+    public MessageConverter messageConverter(ObjectMapper mapper){
+        // 1.定义消息转换器
+        Jackson2JsonMessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter(mapper);
+        // 2.配置自动创建消息id，用于识别不同消息
+        jackson2JsonMessageConverter.setCreateMessageIds(true);
+        return jackson2JsonMessageConverter;
+    }
+
+    @Bean
+    public RabbitMqHelper rabbitMqHelper(RabbitTemplate rabbitTemplate){
+        return new RabbitMqHelper(rabbitTemplate);
     }
 }
